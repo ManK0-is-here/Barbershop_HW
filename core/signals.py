dde7bc11c63423fb3d1912af68478a000edf5846
ManK0-is-here <zarennyjracok@gmail.com>
2025-08-01 02:15:07 +0300

    HW_27: внедрение бота который уведомляет нас в тг о новой заявке

--- core/signals.py
+++ core/signals.py
@@ -1,7 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Review
+from .models import *
 from .mistral import is_bad_review
+from django.db.models.signals import m2m_changed
+from .telegram import send_telegram_message
 
 @receiver(post_save, sender=Review)
 def check_review(sender, instance, created, **kwargs):
@@ -24,3 +26,21 @@ def check_review(sender, instance, created, **kwargs):
         except Exception:
             instance.ai_checked_status = "ai_checked_false"
             instance.save(update_fields=['ai_checked_status'])
+
+
+@receiver(m2m_changed, sender=Order.services.through)
+def notify_new_order(sender, instance, action, **kwargs):
+
+    if action == "post_add":
+        services = "\n".join([f"- {service.name}" for service in instance.services.all()])
+        
+        message = (
+            f"*Новый заказ!* 🎉\n"
+            f"*Клиент:* {instance.client_name}\n"
+            f"*Телефон:* `{instance.phone}`\n"
+            f"*Мастер:* {instance.master.name if instance.master else 'Не назначен'}\n"
+            f"*Дата:* {instance.appointment_date.strftime('%d.%m.%Y %H:%M')}\n"
+            f"*Услуги:*\n{services}"
+        )
+        
+        send_telegram_message(message)            
---
+++ core/telegram.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+import requests
+
+def send_telegram_message(text: str):
+
+    url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": settings.TELEGRAM_CHAT_ID,
+        "text": text,
+        "parse_mode": "Markdown"
+    }
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"Ошибка отправки сообщения в Telegram: {e}")
+        return None
+
