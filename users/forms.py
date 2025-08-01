02e14ad128056ef06499d210ecb4fc80adbc3bbe
ManK0-is-here <zarennyjracok@gmail.com>
2025-08-02 01:04:45 +0300

    HW_31: Изменение форм моделей вьюше и урлов под новые шаблоны

--- users/forms.py
+++ users/forms.py
@@ -5,6 +5,9 @@ from django.contrib.auth.forms import (
 )
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm, SetPasswordForm
+from .models import Profile
+
 
 
 class UserRegisterForm(UserCreationForm):
@@ -51,4 +54,86 @@ class UserLoginForm(AuthenticationForm):
         self.fields['password'].widget.attrs.update({
             'class': 'form-control',
             'placeholder': 'Пароль'
+        })
+
+
+class UserProfileUpdateForm(forms.ModelForm):
+
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+
+        model = Profile
+        fields = ['avatar', 'birth_date', 'telegram_id', 'github_id']
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'telegram_id': forms.TextInput(attrs={'class': 'form-control'}),
+            'github_id': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].initial = self.instance.user.username
+        self.fields['email'].initial = self.instance.user.email
+    
+    def save(self, commit=True):
+
+        profile = super().save(commit=False)
+        profile.user.username = self.cleaned_data['username']
+        profile.user.email = self.cleaned_data['email']
+
+        if commit:
+            profile.user.save()
+            profile.save()
+
+        return profile
+
+
+class UserPasswordChangeForm(PasswordChangeForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['old_password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите текущий пароль:'
+        })
+
+        self.fields['new_password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите новый пароль:'
+        })
+
+        self.fields['new_password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Повторите новый пароль:'
+        })
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Email'
+        })
+
+
+class CustomSetPasswordForm(SetPasswordForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['new_password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Новый пароль'
+        })
+        
+        self.fields['new_password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Подтверждение нового пароля'
         })
--- users/views.py
+++ users/views.py
@@ -1,10 +1,34 @@
-from .forms import UserRegisterForm, UserLoginForm
-from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import *
-from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .models import Profile
+from .forms import ( 
+    UserRegisterForm, 
+    UserLoginForm, 
+    CustomPasswordResetForm, 
+    CustomSetPasswordForm,
+    UserPasswordChangeForm,
+    UserProfileUpdateForm,
+)
+from django.views.generic import (
+    CreateView,  
+    DetailView, 
+    UpdateView,
+)
+from django.contrib.auth.views import (
+    PasswordChangeView,  
+    PasswordResetView, 
+    PasswordResetDoneView, 
+    PasswordResetConfirmView, 
+    PasswordResetCompleteView,
+)
 
 
-class CustomLoginView(LoginView):
+
+class UserLoginView(LoginView):
 
     form_class = UserLoginForm
     template_name = 'users/login.html'
@@ -14,16 +38,108 @@ class CustomLoginView(LoginView):
         return reverse_lazy('landing')
 
 
-class CustomLogoutView(LogoutView):
+class UserLogoutView(LogoutView):
+
     template_name = 'users/logout.html'
     next_page = reverse_lazy('logout')
 
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, "Вы вышли, надеюсь вас не собьёт автобус")
+        return super().dispatch(request, *args, **kwargs)
 
-class RegisterView(CreateView):
+
+class UserRegisterView(CreateView):
 
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('login')
     
     def form_valid(self, form):
+
+        response = super().form_valid(form)
+        user = self.object
+        login(self.request, user)
+
+        return response
+    
+    def dispatch(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            return redirect(self.success_url)
+        
+        return super().dispatch(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+
+        response = super().form_valid(form)
+        user = self.object
+        login(self.request, user)
+        messages.success(self.request, "Вы успешно зарегистрировались! Добро пожаловать в клуб приятель!")
+        
+        return response
+    
+
+class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+
+    form_class = UserPasswordChangeForm
+    template_name = 'users/password_change_form.html'
+    success_url = reverse_lazy('profile_detail')
+
+
+class CustomPasswordResetView(PasswordResetView):
+
+    form_class = CustomPasswordResetForm
+    template_name = 'users/password_reset_form.html'
+    email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('password_reset_done')
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+
+    form_class = CustomSetPasswordForm
+    template_name = 'users/password_reset_confirm.html'
+    success_url = reverse_lazy('password_reset_complete')
+
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    template_name = 'users/password_reset_complete.html'
+
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'users/password_reset_done.html'
+
+
+class UserProfileDetailView(LoginRequiredMixin, DetailView):
+
+    model = Profile
+    template_name = 'users/profile_detail.html'
+    context_object_name = 'profile'
+    
+    def get_object(self):
+        return self.request.user.profile
+
+
+class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
+
+    model = Profile
+    form_class = UserProfileUpdateForm
+    template_name = 'users/profile_update_form.html'
+    success_url = reverse_lazy('profile_detail')
+    
+    def get_object(self):
+        return self.request.user.profile
+    
+    def form_valid(self, form):
+        messages.success(self.request, "Профиль успешно обновлен!")
+        return super().form_valid(form)
+    
+
+class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    
+    form_class = UserPasswordChangeForm
+    template_name = 'users/password_change_form.html'
+    success_url = reverse_lazy('profile_detail')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Пароль успешно изменен!")
         return super().form_valid(form)
