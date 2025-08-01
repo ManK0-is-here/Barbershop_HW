bb418a394a839bc7041a1965edfca1a300758cc0
ManK0-is-here <zarennyjracok@gmail.com>
2025-08-01 18:46:28 +0300

    HW_30: Изменены формы в юзерс

--- users/forms.py
+++ users/forms.py
@@ -4,13 +4,51 @@ from django.contrib.auth.forms import (
     AuthenticationForm,
 )
 from django.contrib.auth.models import User
-# from django.contrib.auth import get_user_model
-# user_model = get_user_model()
+from django.core.exceptions import ValidationError
+
 
 class UserRegisterForm(UserCreationForm):
+
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'})
+    )
+    
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
+        
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': self.fields[fieldname].label
+            })
+            
+    def clean_email(self):
+
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Такой email уже заренистрирован.")
+        
+        return email
+
 
 class UserLoginForm(AuthenticationForm):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Имя пользователя'
+        })
+        
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Пароль'
+        })
